4877619253f7cb0bce403e6414aa0589b47eeea0
LucasX-NZ <24160ls@hvhs.school.nz>
2026-07-23 12:39:38 +0000

    Jul 23, 2026, 12:39 PM

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ let cart = [];
 var cartTotal  = 0;
 let name = 0;
 let balance = 0;
-
+let OUTPUT = document.getElementById("cartoutput")
+let i = 0;
 
 //Functions
 
 function addtocart(item) {
     cart.push(item);
+}
+
+function displaycart() {
+    for ()
 }
