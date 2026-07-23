cd80e47eabf989cde9f634f9440ce636a99e4c4a
LucasX-NZ <24160ls@hvhs.school.nz>
2026-07-23 15:21:00 +0000

    Jul 23, 2026, 3:21 PM

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
+console.log("JavaScript loaded");
 //Variable
 
 let cart = [];
 var cartTotal  = 0;
 let name = 0;
 let balance = 0;
-let OUTPUT = document.getElementById("cartoutput")
-let i = 0;
 
 //Functions
 
@@ -21,17 +20,25 @@ function addtocart(item, price) {
             displaycart();
             return;
         }
+    }
 
+    cart.push(item);
+    cart.push(1);
+    cart.push(price);
 }
 
 function displaycart() {
 
+    let OUTPUT = document.getElementById("cartoutput")
+
     OUTPUT.innerHTML = "";
+
     for (i = 0; i < cart.length;  i += 3) {
 
         OUTPUT.innerHTML +=
         cart[i] +
-        "x" + cart [i + 1] +
+        " x " + cart [i + 1] +
         " : $" + cart [i + 2];
+        "<br><br>";
     }
 }
